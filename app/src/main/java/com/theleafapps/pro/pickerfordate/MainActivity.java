e1c0562d15487e30ed3e0531a8909ae623be7a1d
Abhijeet Singh <redefinedabhi2@gmail.com>
2020-08-30 18:04:07 +0530

    DatePicker Fragment layout and code added

--- app/src/main/java/com/theleafapps/pro/pickerfordate/MainActivity.java
+++ app/src/main/java/com/theleafapps/pro/pickerfordate/MainActivity.java
@@ -1,14 +1,28 @@
 package com.theleafapps.pro.pickerfordate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button picker_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picker_btn = findViewById(R.id.picker_btn);
+        picker_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment dialogFragment = new DatePickerFragment();
+                dialogFragment.show(getSupportFragmentManager(),"datePicker");
+            }
+        });
     }
 }
